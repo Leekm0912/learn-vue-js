f8e364b6cb689d6959e7c47cdae550f9b116bdad
Lee Kyung-min <42348176+Leekm0912@users.noreply.github.com>
2022-08-09 10:23:11 +0900

    commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-world msg="Welcome to Your Vue.js App!" />
+    <button v-on:click="testMethod">클릭</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    testMethod: function () {
+      console.log("testMethod");
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  method: {
+    addNum: function () {
+      console.log("addNum");
+    },
+  },
+};
+</script>
+
+<style></style>
